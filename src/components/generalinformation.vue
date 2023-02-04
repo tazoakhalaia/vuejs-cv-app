c463bfd1d2c1c0c309c3713c3890b666e1bc14c0
tazo <darknemiroff@gmail.com>
2023-02-04 17:52:25 +0400

    resume-page

--- src/components/generalinformation.vue
+++ src/components/generalinformation.vue
@@ -4,7 +4,7 @@
             <div class="private-info">
                 <div class="private-info-header">
                     <div class="img-info-title">
-                        <router-link to="/"><img class="gofirstpageimg" src="../assets/gofirstpagebtn.png"></router-link>
+                        <router-link to="/"><img @click="goFirstPage" class="gofirstpageimg" src="../assets/gofirstpagebtn.png"></router-link>
                     <h1 class="info-title">პირადი ინფო</h1>
                     </div>
                     <h3 class="page-number">1/3</h3>
@@ -23,17 +23,33 @@
                             <h6 class="warning">მინიმუმ 2 ასო, ქართული ასოები</h6>
                         </div>
                     </div>
+                    <div class="upload-picture">
+                        <span class="uploadphoto">პირადი ფოტოს ატვირთვა</span><input type="file" @change="fleupload" >
+                    </div>
+                    <div class="about-me">
+                        <h1>ჩემს შესახებ (არასავალდებულო)</h1>
+                        <textarea name="aboutme"  cols="30" rows="10" v-model="textareaText" maxlength="1000" placeholder="ზოგადი ინფო შენს შესახებ"></textarea>
+                    </div>
+                    <div class="email-field">
+                        <h1>ელ.ფოსტა</h1>
+                       <input type="text" v-model="email" :class="[email.match(emailRegex) ? valid : emailinput]" placeholder="ელ.ფოსტა">
+                    </div>
+                    <div class="phone-number">
+                        <h1>მობილური ნომერი</h1>
+                       <input type="text" v-model="phoneText" :class="[phoneText.match(phoneRegex) ? valid : phoneinput]" placeholder="ტელეფონის ნომერი">
+                    </div>
                         <button class="submit-btn" type="submit" :disabled="!isDisabled">შემდეგი</button>
                     </form>
             </div>
         </div>
         <div class="resume-design">
-            <h1>{{ name }}</h1>
+            <Resume :nameOfUser ="name" :surname="surname" :userImage="picture" :aboutUser="textareaText" :userEmail="email" :userNumber="phoneText"/>
         </div>
     </div>
 </template>
 <script>
 import Vector from '../assets/Vector.png'
+import Resume from '../resume/cv.vue'
 export default {
     data(){
         return {
@@ -42,10 +58,20 @@ export default {
             valid: "validname-surname",
             name: "",
             surname: "",
+            textareaText: "",
+            email: "",
             geoRegex: /^[ა-ჰ]+$/g,
-            vector: [Vector]
-
+            emailRegex: /^\w+([\.-]?\w+)*@redberry.ge/,
+            vector: [Vector],
+            picture: null,
+            emailinput: "email-field input",
+            phoneRegex: /^(\+995\d{9})$/,
+            phoneText: "",
+            phoneinput: "phone-number input"
         }
+    },
+    components: {
+        Resume
     },
     watch: {
         surname(newMessage){
@@ -53,15 +79,29 @@ export default {
         },
         name(newName){
             localStorage.setItem('name', newName)
+        },
+        picture(newImage){
+            localStorage.setItem('image', newImage)
+        },
+        email(newEmail){
+            localStorage.setItem("email",newEmail)
+        },
+        phoneText(newNumber){
+            localStorage.setItem("phone_number", newNumber)
         }
     },
     mounted(){
-       this.surname = localStorage.getItem("surname")
-       this.name = localStorage.getItem("name")
+       this.surname = localStorage.getItem("surname") || ""
+       this.name = localStorage.getItem("name") || ""
+       this.picture = localStorage.getItem('image') || ""
+       this.email = localStorage.getItem("email") || ""
+       this.phoneText = localStorage.getItem("phone_number") || ""
     },
     computed: {
         isDisabled(){
-            if(this.name.match(this.geoRegex) && this.name.length >= 2 && this.surname.match(this.geoRegex) && this.surname.length >= 2){
+            if(this.name.match(this.geoRegex) && this.name.length >= 2 
+            && this.surname.match(this.geoRegex) && this.surname.length >= 2 
+            && this.email.match(this.emailRegex) && this.phoneText.match(this.phoneRegex) ){
                 return true
             }else {
                 return false
@@ -69,6 +109,17 @@ export default {
         }
     },
     methods: {
+        fleupload(e){
+            this.picture = e.target.files[0]
+            var reader = new FileReader()
+            reader.addEventListener("load", () => {
+                 this.picture = reader.result;
+                 })
+                 reader.readAsDataURL(this.picture);
+                },
+        goFirstPage(){
+            localStorage.clear()
+        },
         foo(e){
             e.preventDefault();
             console.log('nice');
@@ -77,6 +128,11 @@ export default {
 }
 </script>
 <style scoped>
+
+* {
+    box-sizing: border-box;
+    font-family: HelveticaNeue;
+}
 
 @font-face {
       font-family: HelveticaNeue;
@@ -134,8 +190,8 @@ hr {
 form {
     position: relative;
     width: 100%;
-    margin-top: 15px;
-    padding-bottom: 100%;
+    margin-top: 85px;
+    padding-bottom: 50%;
 }
 
 .name-surname {
@@ -156,7 +212,7 @@ form {
     width: 390px;
     height: 31px;
     border-radius: 3px;
-    border: none;
+    border: 1px solid #76f776;
     outline: none;
     background: url('../assets/Vector.png');
     background-color: white;
@@ -191,6 +247,93 @@ form {
     cursor: pointer;
     outline: none;
     border: none;
+    margin-top: 100px;
 }
 
+.uploadphoto  {
+    font-family: HelveticaNeue;
+    font-weight: bold;
+}
+
+.upload-picture {
+    margin-top: 90px;
+}
+
+.upload-picture input {
+    margin-left: 15px;
+}
+
+.upload-picture input[type="file"]{
+    position: relative;
+    width: 110px;
+    height: 20;
+    color: rgba(218, 17, 17, 0);
+    background-color: #0E80BF;
+    border-radius: 3px;
+}
+
+.upload-picture input[type="file"]::before {
+    position: absolute;
+    content: 'ატვირთე';
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    color: white;
+    white-space: nowrap; 
+    cursor: pointer;
+    font-family: HelveticaNeue;
+    font-size: 12px;
+}
+
+input[type="file"]::-webkit-file-upload-button {
+    visibility: hidden;
+  }
+
+  .about-me {
+    margin-top: 100px;
+  }
+
+  .about-me h1 {
+    font-size: 18px;
+  }
+
+  .about-me textarea {
+    resize: none;
+    margin-top: 5px;
+    width: 100%;
+    border-radius: 3px;
+    outline: none;
+  }
+
+  .email-field {
+    margin-top: 110px;
+  }
+  .email-field h1 {
+    font-size: 18px;
+  }
+
+  .email-field input {
+    width: 100%;
+    height: 35px;
+    border-radius: 3px;
+    outline: none;
+    border: 0.5px solid grey;
+    margin-top: 10px;
+  }
+  .phone-number {
+    margin-top: 110px;
+  }
+
+  .phone-number input {
+    width: 100%;
+    height: 35px;
+    border-radius: 3px;
+    outline: none;
+    border: 0.5px solid grey;
+    margin-top: 10px;
+  }
+
+  .phone-number h1 {
+    font-size: 18px;
+  }
 </style>
---
+++ src/resume/cv.vue
@@ -0,0 +1,79 @@
+<template>
+    <div style="display: flex; justify-content: center;">
+        <div class="resume-container">
+            <div class="left-side">
+                <h1 class="username">{{ nameOfUser }} {{ surname }}</h1>
+                <div class="user-emssssail">
+                    <h3>{{ userEmail }}</h3>
+                    <h3>{{ userNumber }}</h3>
+                </div>
+                <div class="about-user">
+                    {{ aboutUser }}
+                </div>
+            </div>
+        <img v-if="userImage" :src="userImage" class="userimage" >
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    props: {
+        nameOfUser: {
+            type: String,
+            require: false
+        },
+        surname: {
+            type: String,
+            require: false
+        },
+        userImage: {
+            type: Image,
+            require: false
+        },
+        aboutUser: {
+            type: String,
+            require: false
+        },
+        userEmail: {
+            type: String,
+            require: false
+        },
+        userNumber: {
+            type: String,
+            require: false
+        }
+    }
+}
+</script>
+<style scoped>
+
+.resume-container {
+    width: 100%;
+    padding-top: 50px;
+}
+
+.username {
+    font-size: 34px;
+    font-weight: bold;
+    color: #F93B1D;
+
+}
+
+.left-side {
+    float: left;
+    width: 50%;
+    word-wrap: break-word ;
+}
+
+.userimage {
+    margin-top: 0;
+    margin-left: 10px;
+    width: 220px;
+    height: 220px;
+    border-radius: 50%;
+}
+</style>
